e95d8e815300ea5f438f2f5b964274451df4225a
Jai Bhagat <jaybhagat841@gmail.com>
2022-11-25 14:28:47 -0500

    server: sample get and post endpoints

--- server/app.js
+++ server/app.js
@@ -1,8 +1,35 @@
-const express = require('express');
+const bodyParser = require("body-parser")
+const express = require("express");
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json({ hello: 'world' });
+const db = { users: [{ id: 123, user: "@ChaiWithJai", name: "Jai Bhagat" }] };
+
+app.get("/", (req, res) => {
+  res.json({ hello: "world" });
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const user = db.users.find((u) => u.id === id);
+
+  if (!user) {
+    return res.status(404).send(`User with id:  ${id} not found.`);
+  }
+
+  res.json(user);
+});
+
+app.post("/users/", ({body}, res) => {
+  // TODO:  generate nano id later
+  const id = Math.floor(Math.random() * 10_000);
+  const user = {...body, id};
+
+  // Refactoring:  use ORM and database later
+  db.users.push(user);
+
+  res.json(user);
 });
 
 module.exports = app;
--- server/app.spec.js
+++ server/app.spec.js
@@ -1,10 +1,61 @@
-const request = require('supertest');
-const app = require('./app');
+const request = require("supertest");
+const app = require("./app");
 
-describe('/', () => {
+describe("GET / responds with hello world", () => {
   test('should return {hello: "world" }', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get("/");
     expect(res.status).toEqual(200);
-    expect(res.body).toEqual({ hello: 'world' });
+    expect(res.body).toEqual({ hello: "world" });
+  });
+});
+
+describe("GET /users/:id", () => {
+  test("should respond with user record", async () => {
+    // arrange
+    const api = request(app);
+    const userId = 123;
+    const route = `/users/${userId}`;
+
+    // act
+    const response = await api.get(route);
+
+    // assert
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual({
+      id: 123,
+      user: "@ChaiWithJai",
+      name: "Jai Bhagat",
+    });
+  });
+
+  test("should respond with 404 if user record does not exist", async () => {
+    // arrange
+    const api = request(app);
+    const userId = 124;
+    const route = `/users/${userId}`;
+
+    // act
+    const response = await api.get(route);
+
+    // assert
+    expect(response.status).toEqual(404);
+    expect(response.error.text).toEqual(`User with id:  ${userId} not found.`);
+  });
+});
+
+describe("POST /users/:id", () => {
+  test("should respond with newly created user record using JSON payload", async () => {
+    // arrange
+    const api = request(app);
+    const payload = {name: 'Pablo Picasso', user: '@piz_piz'};
+    const route = `/users`;
+
+    // act
+    const response = await api.post(route).send(payload);
+
+    // assert
+    expect(response.status).toEqual(200);
+    expect(response.body.id).toBeDefined();
+    expect(response.body).toEqual(expect.objectContaining(payload));
   });
 });
